fa9833a74af486681c0d6029ea7c94994bec6df1
Artem Kulakov <artem.kulakov@gmail.com>
2020-03-12 22:54:52 +0300

    Do the rest fields in checkout#index view

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -2,14 +2,28 @@ class CheckoutController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @countries = CS.countries
   end
 
   def update_user
-    @user = current_user
-    puts 'foobar'
-    puts params
+    current_user.update(user_params)
   end
 
   def payment
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(
+        :first_name,
+        :last_name,
+        :phone,
+        :street,
+        :apartment,
+        :city,
+        :country,
+        :zip
+      )
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@ User.create(
   email: 'a@a.com',
   password: '123456',
   password_confirmation: '123456'
-  )
+)
