99dfb04b907472c32131595ba13921a8cc7ad9fa
Artem Kulakov <artem.kulakov@gmail.com>
2020-03-16 23:24:31 +0300

    posts#index

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,12 +15,19 @@
 #   )
 # end
 
-20.times do |index|
-  Post.create(
-    title: Faker::Lorem.sentence(word_count: 3),
-    subtitle: Faker::Lorem.sentence(word_count: 9),
-    user_id: index + 2,
-    date: Faker::Date.backward(days: 90),
-    body: Faker::Lorem.sentence(word_count: 70)
+# 20.times do |index|
+#   Post.create(
+#     title: Faker::Lorem.sentence(word_count: 3),
+#     subtitle: Faker::Lorem.sentence(word_count: 9),
+#     user_id: index + 2,
+#     date: Faker::Date.backward(days: 90),
+#     body: Faker::Lorem.sentence(word_count: 70)
+#   )
+# end
+
+User.all.each do |user|
+  user.update(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
   )
 end
