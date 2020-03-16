036135bd2ef16b497f139dfb5f519e649e959cdb
Artem Kulakov <artem.kulakov@gmail.com>
2020-03-16 23:10:01 +0300

    Seed User and Post

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,26 @@
-User.create(
-  email: 'a@a.com',
-  password: '123456',
-  password_confirmation: '123456'
-)
+# User.delete_all
+# Post.delete_all
+
+# User.create(
+#   email: 'a@a.com',
+#   password: '123456',
+#   password_confirmation: '123456'
+# )
+
+# 20.times do
+#   User.create(
+#     email: Faker::Internet.email,
+#     password: '123456',
+#     password_confirmation: '123456'
+#   )
+# end
+
+20.times do |index|
+  Post.create(
+    title: Faker::Lorem.sentence(word_count: 3),
+    subtitle: Faker::Lorem.sentence(word_count: 9),
+    user_id: index + 2,
+    date: Faker::Date.backward(days: 90),
+    body: Faker::Lorem.sentence(word_count: 70)
+  )
+end
