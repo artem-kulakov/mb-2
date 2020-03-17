bf30b0adea9884ed5d90b4016c252ef6d4d3ce82
Artem Kulakov <artem.kulakov@gmail.com>
2020-03-17 22:01:59 +0300

    Show categories

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
     @recent_posts = Post.order(:date).last(4)
+    @categories = Post.categories.to_hash
   end
 
   # GET /posts/1
