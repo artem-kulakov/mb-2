505d5180d8f68558c86d0662b17629c996bf9323
Artem Kulakov <artem.kulakov@gmail.com>
2020-03-18 22:39:25 +0300

    Post body

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,8 @@ Post.all.each do |post|
   post.update(
     # image: "/assets/img/500x250/img#{1+rand(14)}.jpg",
     # category: rand(6),
-    hero: "/assets/img/1920x1080/img#{1+rand(39)}.jpg"
+    # hero: "/assets/img/1920x1080/img#{1+rand(39)}.jpg",
+    body: Faker::Lorem.sentence(word_count: 400)
   )
 end
 
