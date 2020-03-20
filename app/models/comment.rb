e7a1f3f73c874b1d2c50e5bc70b9c0d94533f7cf
Artem Kulakov <artem.kulakov@gmail.com>
2020-03-20 23:55:45 +0300

    Count likes and dislikes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,13 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  has_many :likes
+
+  def likes_count
+    likes.where(value: true).count
+  end
+
+  def dislikes_count
+    likes.where(value: false).count
+  end
 end
