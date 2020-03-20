a10514d7efc6bc2bf06de7febc85ac9589eb4694
Artem Kulakov <artem.kulakov@gmail.com>
2020-03-21 00:55:50 +0300

    Like button calls controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,10 @@
 class CommentsController < ApplicationController
   def like
+    Like.create(
+      user_id: current_user.id,
+      comment_id: params[:comment],
+      value: true
+    )
   end
 
   def dislike
