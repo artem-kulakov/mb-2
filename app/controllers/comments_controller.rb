977007b95496220aaa56a5e1c54f0cebf16d6368
Artem Kulakov <artem.kulakov@gmail.com>
2020-03-21 17:14:05 +0300

    Like/dislike are done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,27 @@
 class CommentsController < ApplicationController
   def like
+    @comment_id = params[:comment]
+
     Like.create(
       user_id: current_user.id,
-      comment_id: params[:comment],
+      comment_id: @comment_id,
       value: true
     )
+
+    comment = Comment.find(@comment_id)
+    @count = comment.likes_count
   end
 
   def dislike
+    @comment_id = params[:comment]
+
     Like.create(
       user_id: current_user.id,
-      comment_id: params[:comment],
+      comment_id: @comment_id,
       value: false
     )
+
+    comment = Comment.find(@comment_id)
+    @count = comment.dislikes_count
   end
 end
